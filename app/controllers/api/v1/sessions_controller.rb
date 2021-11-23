f5fb7a688e83b947023c23e66b391fed55fef057
Mitul Mistry <MitulMistryDev@gmail.com>
2021-11-23 17:19:50 -0600

    Implement creating and destroying sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,24 @@
 class Api::V1::SessionsController < ApplicationController
 
+  # Login action
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      login(@user)
+      render "api/v1/users/show"
+    else
+      render json: ["Invalid username/password"], status: :unauthorized
+    end
   end
 
+  # Logout action
   def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/v1/users/show"
+    else
+      render json: ["Not signed in"], status: :not_found
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+
+  private
+  
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    # Coerces return value into a Boolean
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = @user.id
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+
+  def authorized
+    render json: ["Unauthorized"], status: :unauthorized unless logged_in?
+  end
 end
