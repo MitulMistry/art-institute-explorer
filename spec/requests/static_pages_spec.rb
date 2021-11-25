5c7b69f5f1abfcaef12047d335c5e75b505a595f
Mitul Mistry <MitulMistryDev@gmail.com>
2021-11-25 00:49:21 -0600

    Add test for static index page request

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "serves the static index page" do
+      get root_path
+      expect(response).to be_successful
+      
+      # Check for HTML title
+      expect(response.body).to include("Art Institute Explorer")
+    end
   end
 end
