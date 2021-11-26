4e09aeae3c916622935749812647d85ed80669d8
Mitul Mistry <MitulMistryDev@gmail.com>
2021-11-26 00:15:03 -0600

    Rename SavedArtworks route tests to ArtworkSaves

--- spec/routing/artwork_saves_routing_spec.rb
+++ spec/routing/artwork_saves_routing_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
-RSpec.describe Api::V1::SavedArtworksController, type: :routing do
+RSpec.describe Api::V1::ArtworkSavesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/api/v1/saved_artworks").to route_to("api/v1/saved_artworks#index", format: :json)
+      expect(get: "/api/v1/artwork_saves").to route_to("api/v1/artwork_saves#index", format: :json)
     end
 
     it "routes to #create" do
-      expect(post: "/api/v1/saved_artworks").to route_to("api/v1/saved_artworks#create", format: :json)
+      expect(post: "/api/v1/artwork_saves").to route_to("api/v1/artwork_saves#create", format: :json)
     end
 
     it "routes to #destroy" do
-      expect(delete: "/api/v1/saved_artworks/1").to route_to("api/v1/saved_artworks#destroy", id: "1", format: :json)
+      expect(delete: "/api/v1/artwork_saves/1").to route_to("api/v1/artwork_saves#destroy", id: "1", format: :json)
     end
   end
 end
