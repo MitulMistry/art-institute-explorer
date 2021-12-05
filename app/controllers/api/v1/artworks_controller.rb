c60dbdf963d22c2c0926c34e9fa049f32657fa74
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-04 20:14:08 -0600

    Fix formatting of URIs and response status codes for Faraday API requests in ArtworksController

--- app/controllers/api/v1/artworks_controller.rb
+++ app/controllers/api/v1/artworks_controller.rb
@@ -5,21 +5,15 @@ class Api::V1::ArtworksController < ApplicationController
   def index
     begin
       url = "https://api.artic.edu/api/v1/artworks"
+
       response = Faraday.get(url) do |req|
         req.params["limit"] = 10
-        req.params["fields"] =
-        [
-          "id",
-          "title",
-          "artist_display",
-          "date_display",
-          "main_reference_number",
-          "image_id"
-        ]
+        req.params["fields"] = "id,title,artist_title,date_display,main_reference_number,image_id"
         req.options.timeout = 8000
       end
+
       json = JSON.parse(response.body)
-      render json: json
+      render json: json, status: response.status
 
     rescue Faraday::TimeoutError
       render_timeout
@@ -30,22 +24,16 @@ class Api::V1::ArtworksController < ApplicationController
     begin
       query = params[:q]
       url = "https://api.artic.edu/api/v1/artworks/search"
+
       response = Faraday.get(url) do |req|
         req.params["limit"] = 10
         req.params["q"] = query
-        req.params["fields"] =
-        [
-          "id",
-          "title",
-          "artist_display",
-          "date_display",
-          "main_reference_number",
-          "image_id"
-        ]
+        req.params["fields"] = "id,title,artist_title,date_display,main_reference_number,image_id"
         req.options.timeout = 8000
       end
+
       json = JSON.parse(response.body)
-      render json: json
+      render json: json, status: response.status
 
     rescue Faraday::TimeoutError
       render_timeout
@@ -55,12 +43,14 @@ class Api::V1::ArtworksController < ApplicationController
   def show
     begin
       id = params[:id]
-      url = "https://api.artic.edu/api/v1/artworks/" + id
+      url = "https://api.artic.edu/api/v1/artworks/#{id}"
+      
       response = Faraday.get(url) do |req|
         req.options.timeout = 8000
       end
+
       json = JSON.parse(response.body)
-      render json: json
+      render json: json, status: response.status
 
     rescue Faraday::TimeoutError
       render_timeout
