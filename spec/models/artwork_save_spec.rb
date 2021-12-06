0b6cecf1e1ed0e46986d6f639a810fcce5df9aae
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-06 11:42:54 -0600

    Add tests for ArtworkSave and Artwork creation class methods

--- spec/models/artwork_save_spec.rb
+++ spec/models/artwork_save_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe ArtworkSave, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:artwork) }
   end
+
+  describe "class methods" do
+    context "##new_by_aic_id"
+    it "returns a valid new ArtworkSave using an aic_id" do
+      VCR.use_cassette("artwork_save_grande_jatte") do
+        user = create(:user)
+        expect(ArtworkSave.new_by_aic_id(user.id, 27992)).to be_valid
+      end
+    end
+  end
 end
--- spec/models/artwork_spec.rb
+++ spec/models/artwork_spec.rb
@@ -29,4 +29,46 @@ RSpec.describe Artwork, type: :model do
       it { should validate_length_of(:alt_text).is_at_most(1000) }
     end
   end
+
+  describe "class methods" do
+    context "##find_or_create_by_aic_id" do
+      before :each do
+        @aic_id = 27992
+      end
+
+      it "finds the correct Artwork using an aic_id if it exists" do
+        VCR.use_cassette("artwork_save_grande_jatte") do
+          artwork = create(:artwork, aic_id: @aic_id)
+          found_artwork = Artwork.find_or_create_by_aic_id(@aic_id)
+          expect(found_artwork).to be_valid
+          expect(found_artwork.aic_id).to eq(@aic_id)
+        end
+      end
+
+      it "doesn't create an Artwork if it exists" do
+        VCR.use_cassette("artwork_save_grande_jatte") do
+          artwork = create(:artwork, aic_id: @aic_id)
+          expect {
+            Artwork.find_or_create_by_aic_id(@aic_id)
+          }.to change(Artwork, :count).by(0)
+        end
+      end
+      
+      it "creates an Artwork using an aic_id if it doesn't exist" do
+        VCR.use_cassette("artwork_save_grande_jatte") do
+          expect {
+            Artwork.find_or_create_by_aic_id(@aic_id)
+          }.to change(Artwork, :count).by(1)
+        end
+      end
+
+      it "returns the correctly created Artwork using an aic_id if it doesn't exist" do
+        VCR.use_cassette("artwork_save_grande_jatte") do
+          artwork = Artwork.find_or_create_by_aic_id(@aic_id)
+          expect(artwork).to be_valid
+          expect(artwork.aic_id).to eq(@aic_id)
+        end
+      end
+    end
+  end
 end
