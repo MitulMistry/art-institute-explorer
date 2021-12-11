c1da35ea5e45cb90fb0c55ff02acdcfe52a780b5
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-11 00:27:02 -0600

    Add default Rails  wildcard route and test to work with React BrowserRouter and ensure static page with JS is served on non-API requests

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   end
 
   root to: 'static_pages#root'
+  get '*path', to: 'static_pages#root'
 end
--- spec/routing/static_pages_routing_spec.rb
+++ spec/routing/static_pages_routing_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe StaticPagesController, type: :routing do
     it "routes to #root" do
       expect(get: "/").to route_to("static_pages#root")
     end
+
+    it "routes default to #root" do
+      expect(get: "/test123").to route_to(controller: "static_pages", action: "root", path: "test123")
+      expect(get: "/artworks/1").to route_to(controller: "static_pages", action: "root", path: "artworks/1")
+      expect(get: "/users/1").to route_to(controller: "static_pages", action: "root", path: "users/1")
+    end
   end
 end
