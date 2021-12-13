ddb640000869c12c0bbd873ca487010d3c5e4fc3
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-12 23:55:42 -0600

    Add initial basic styling for NavBar, Footer, and content

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -22,20 +22,22 @@ import { Footer } from './common/Footer';
 const App = () => (
   <div className="app">
     <NavBar />
-    <Routes>
-      <Route path="/" element={<HomePageContainer />} />
-      <Route path="login" element={<LoginFormContainer />} />        
-      <Route path="signup" element={<SignUpFormContainer />} />        
-      <Route path="users/:userId" element={<UserShowContainer />} />        
-      <Route path="users/:userId/edit" element={<UserEditContainer />} />        
-      <Route path="artworks" element={<ArtworksIndexContainer />} />        
-      <Route path="artworks/:artworkId" element={<ArtworksShowContainer />} />        
-      <Route path="artworks/saved" element={<ArtworksSavedContainer />} />        
-      <Route path="collections" element={<CollectionsIndexContainer />} />        
-      <Route path="collections/new" element={<CollectionsNewContainer />} />        
-      <Route path="collections/:collectionId" element={<CollectionShowContainer />} />        
-      <Route path="collections/:collectionId/edit" element={<CollectionEditContainer />} />        
-    </Routes>
+    <div className="content">
+      <Routes>
+        <Route path="/" element={<HomePageContainer />} />
+        <Route path="login" element={<LoginFormContainer />} />        
+        <Route path="signup" element={<SignUpFormContainer />} />        
+        <Route path="users/:userId" element={<UserShowContainer />} />        
+        <Route path="users/:userId/edit" element={<UserEditContainer />} />        
+        <Route path="artworks" element={<ArtworksIndexContainer />} />        
+        <Route path="artworks/:artworkId" element={<ArtworksShowContainer />} />        
+        <Route path="artworks/saved" element={<ArtworksSavedContainer />} />        
+        <Route path="collections" element={<CollectionsIndexContainer />} />        
+        <Route path="collections/new" element={<CollectionsNewContainer />} />        
+        <Route path="collections/:collectionId" element={<CollectionShowContainer />} />        
+        <Route path="collections/:collectionId/edit" element={<CollectionEditContainer />} />        
+      </Routes>
+    </div>
     <Footer />
   </div>
 );
--- app/javascript/packs/components/nav/NavBar.jsx
+++ app/javascript/packs/components/nav/NavBar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export class NavBar extends React.Component {
   render() {
     return (
-      <div className="navbar">
-        <p>NavBar</p>
-      </div>
+      <header className="navbar">
+        <nav className="header-nav">
+        <div>
+          <h1 className="header-nav-logo"><Link to="/">AIC Explorer</Link></h1>
+          <ul className="header-nav-list clearfix">
+            <li><Link to="login">Login</Link></li>
+            <li><Link to="signup">Signup</Link></li>
+            <li><Link to="artworks">Artworks</Link></li>
+            <li><Link to="artworks/saved">Saved Artworks</Link></li>
+            <li><Link to="collections">Collections</Link></li>
+            <li><Link to="collections/new">New Collection</Link></li>
+          </ul>
+        </div>
+        </nav>
+      </header>
     );
   }
 }
