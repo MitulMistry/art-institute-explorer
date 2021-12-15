71f47d815ca0b1eb1093661b8a9b585d0771a861
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-15 14:51:36 -0600

    Update text for Footer component

--- app/javascript/packs/components/Footer/Footer.jsx
+++ app/javascript/packs/components/Footer/Footer.jsx
@@ -6,8 +6,11 @@ export class Footer extends React.Component {
 
     return (
       <div className="footer">
-        <p><a href="https://github.com/MitulMistry/art-institute-explorer">Github repo for this site</a><br />
-        &copy; Mitul Mistry { currentYear }</p>
+        <p>
+          Website Copyright &copy; Mitul Mistry { currentYear }<br />
+          <a href="https://github.com/MitulMistry/art-institute-explorer">Github repo for this site</a><br />
+          Powered by the <a href="https://www.artic.edu/open-access/public-api">AIC API</a> provided by the <a href="https://www.artic.edu/">Art Institute of Chicago</a>
+        </p>
       </div>
     );
   }
