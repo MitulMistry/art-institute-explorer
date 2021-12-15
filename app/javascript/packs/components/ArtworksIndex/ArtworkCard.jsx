1a0d2dc3ec58c963c36d18cacfac9e8695beda0b
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-15 16:41:38 -0600

    Set up placeholder image for ArtworkCard to prevent crash if no image is available

--- app/javascript/packs/components/ArtworksIndex/ArtworkCard.jsx
+++ app/javascript/packs/components/ArtworksIndex/ArtworkCard.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Placeholder_Image from 'images/No-Image-Placeholder.png';
 
-export const ArtworkCard = ({ artwork, image_base_url }) => {
-  return (
-    <div className="artwork-card">
-      <Link to={`${artwork.id}`}>
-        <img src={`${image_base_url}/${artwork.image_id}/full/843,/0/default.jpg`} 
-          alt={artwork.thumbnail.alt_text} className="artwork-card-image" />
-      </Link>
-      <h5 className="artwork-card-title">
-        <Link to={`${artwork.id}`}>{artwork.title}</Link>
-      </h5>
-      <p className="artwork-card-artist">{artwork.artist_title}</p>
-    </div>
-  );
-};
+export class ArtworkCard extends React.Component {
+  render() {
+    const { artwork, image_base_url } = this.props;
+
+    // Check if image is available, otherwise use placeholder so
+    // application doesn't crash.
+    let image = null;
+    if (artwork.image_id && artwork.thumbnail.alt_text) {
+      image = (
+        <img src={`${image_base_url}/${artwork.image_id || 0}/full/843,/0/default.jpg`} 
+          alt={artwork.thumbnail.alt_text || "Artwork"} className="artwork-card-image" />
+      );
+    } else {
+      image = (
+        <img src={Placeholder_Image} 
+          alt="Placeholder image" className="artwork-card-image" />
+      );
+    }
+
+    return (
+      <div className="artwork-card">
+        <Link to={`${artwork.id}`}>
+          {image}
+        </Link>
+        <h5 className="artwork-card-title">
+          <Link to={`${artwork.id}`}>{artwork.title}</Link>
+        </h5>
+        <p className="artwork-card-artist">{artwork.artist_title}</p>
+      </div>
+    );
+  }
+}
