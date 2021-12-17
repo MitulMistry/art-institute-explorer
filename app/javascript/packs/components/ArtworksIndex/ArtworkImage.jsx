f0357081060dc24dd71d199c6885abfb6fd75c69
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-17 00:26:14 -0600

    Add images to CollectionCards in CollectionsIndex

--- app/javascript/packs/components/ArtworksIndex/ArtworkImage.jsx
+++ app/javascript/packs/components/ArtworksIndex/ArtworkImage.jsx
@@ -4,13 +4,23 @@ import Placeholder_Image from 'images/No-Image-Placeholder.png';
 export class ArtworkImage extends React.Component {
   render() {
     const { artwork, imageBaseUrl } = this.props;
-
-    // Check if image is available, otherwise use placeholder.
+    
     let image = null;
-    if (artwork.image_id && artwork.thumbnail.alt_text) {
+    let altText = null;
+
+    // altText is stored in nested thumbnail key for AIC API calls, but
+    // stored in alt_text key for internal API calls for saved Artworks.
+    if ("thumbnail" in artwork) {
+      altText = artwork.thumbnail.alt_text;
+    } else if ("alt_text" in artwork) {
+      altText = artwork.alt_text
+    }
+    
+    // Check if image is available, otherwise use placeholder.
+    if (artwork.image_id) {
       image = (
         <img src={`${imageBaseUrl}/${artwork.image_id || 0}/full/843,/0/default.jpg`} 
-          alt={artwork.thumbnail.alt_text || "Artwork"} className="artwork-image" />
+          alt={altText || "Artwork"} className="artwork-image" />
       );
     } else {
       image = (
--- app/javascript/packs/components/CollectionsIndex/CollectionCard.jsx
+++ app/javascript/packs/components/CollectionsIndex/CollectionCard.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ArtworkImage } from '../ArtworksIndex/ArtworkImage';
 
 export class CollectionCard extends React.Component {
   render() {
     const { collection } = this.props;
 
+    // Select first Artwork in Collection to use as image, or
+    // else provide an empty object to trigger a placeholder image.
+    let image = null;
+    if (collection.artworks.length > 0) {
+      image = (
+        <Link to={`${collection.id}`}>
+          <ArtworkImage
+            artwork={collection.artworks[0]}
+            imageBaseUrl={collection.artworks[0].image_url_prefix}
+          />
+        </Link>
+      )
+    } else {
+      const emptyArtwork = {};
+      image = (
+        <Link to={`${collection.id}`}>
+          <ArtworkImage
+            artwork={emptyArtwork}
+          />
+        </Link>
+      )
+    }
+
     return (
       <div className="collection-card masonry-item">
+        {image}
         <h5 className="collection-card-title">
           <Link to={`${collection.id}`}>{collection.title}</Link>
         </h5>
-        <p className="collection-card-user">{collection.user.username}</p>
+        <p className="collection-card-user">
+          <Link to={`/users/${collection.user.id}`}>{collection.user.username}</Link>
+        </p>
       </div>
     );
   }
