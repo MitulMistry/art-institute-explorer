a4cda28e862506d37215e05566a49ec15ef9855a
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-22 00:11:37 -0600

    Implement protected and auth routes for Reacter Router v6

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -17,6 +17,7 @@ import CollectionsIndexContainer from './CollectionsIndex/CollectionsIndexContai
 import CollectionsNewContainer from './CollectionNew/CollectionNewContainer';
 import CollectionShowContainer from './CollectionShow/CollectionShowContainer';
 import CollectionEditContainer from './CollectionEdit/CollectionEditContainer';
+import { ProtectedRoute, AuthRoute } from './routeUtil/routeUtil';
 
 const App = () => (
   <div className="app">
@@ -24,17 +25,41 @@ const App = () => (
     <div className="content">
       <Routes>
         <Route path="/" element={<HomePageContainer />} />
-        <Route path="login" element={<LoginFormContainer />} />        
-        <Route path="signup" element={<SignUpFormContainer />} />        
-        <Route path="users/:userId" element={<UserShowContainer />} />        
-        <Route path="users/:userId/edit" element={<UserEditContainer />} />        
-        <Route path="artworks" element={<ArtworksIndexContainer />} />        
-        <Route path="artworks/:artworkId" element={<ArtworkShowContainer />} />        
-        <Route path="artworks/saved" element={<ArtworksSavedContainer />} />        
-        <Route path="collections" element={<CollectionsIndexContainer />} />        
-        <Route path="collections/new" element={<CollectionsNewContainer />} />        
-        <Route path="collections/:collectionId" element={<CollectionShowContainer />} />        
-        <Route path="collections/:collectionId/edit" element={<CollectionEditContainer />} />        
+        <Route path="login" element={
+          <AuthRoute>
+            <LoginFormContainer />
+          </AuthRoute>
+        } />
+        <Route path="signup" element={
+          <AuthRoute>
+            <SignUpFormContainer />
+          </AuthRoute>
+        } />
+        <Route path="users/:userId" element={<UserShowContainer />} />
+        <Route path="users/:userId/edit" element={
+        <ProtectedRoute>
+          <UserEditContainer />
+        </ProtectedRoute>
+        } />
+        <Route path="artworks" element={<ArtworksIndexContainer />} />
+        <Route path="artworks/:artworkId" element={<ArtworkShowContainer />} />
+        <Route path="artworks/saved" element={
+          <ProtectedRoute>
+            <ArtworksSavedContainer />
+          </ProtectedRoute>
+          } />
+        <Route path="collections" element={<CollectionsIndexContainer />} />
+        <Route path="collections/new" element={
+          <ProtectedRoute>
+            <CollectionsNewContainer />
+          </ProtectedRoute>
+          } />
+        <Route path="collections/:collectionId" element={<CollectionShowContainer />} />
+        <Route path="collections/:collectionId/edit" element={
+          <ProtectedRoute>
+            <CollectionEditContainer />
+          </ProtectedRoute>
+          } />
       </Routes>
     </div>
   </div>
---
+++ app/javascript/packs/components/routeUtil/routeUtil.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useLocation, Navigate } from 'react-router-dom';
+
+// Referenced from: https://reactrouter.com/docs/en/v6/examples/auth
+// https://stackblitz.com/github/remix-run/react-router/tree/main/examples/auth?file=src/App.tsx
+
+function ProtectedRouteComponent({ children, loggedIn }) {
+  if (!loggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+function AuthRouteComponent({ children, loggedIn }) {
+  if (loggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
+
+const mapStateToProps = state => (
+  {loggedIn: Boolean(state.session.id)}
+);
+
+export const ProtectedRoute = connect(mapStateToProps)(ProtectedRouteComponent);
+
+export const AuthRoute = connect(mapStateToProps)(AuthRouteComponent);
