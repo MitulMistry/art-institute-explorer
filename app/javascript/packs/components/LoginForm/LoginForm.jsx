6b57db257fb1082277db289c680692c7177799f3
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-21 19:40:48 -0600

    Implement LoginForm component and functionality

--- app/javascript/packs/components/LoginForm/LoginForm.jsx
+++ app/javascript/packs/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,90 @@
 import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { RenderErrors } from '../RenderErrors/RenderErrors';
 
 export class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const { errors, resetSessionErrors } = this.props;
+    if (Object.keys(errors).length > 0) {
+      resetSessionErrors();
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <div>
+      <RenderErrors
+        errors={this.props.errors}
+      />
+      </div>
+    );
+  }
+
   render() {
+    const { redirect, resetRedirect } = this.props;
+
+    if (redirect) {
+      resetRedirect();
+      return (
+        <Navigate to={redirect} replace={true} />
+      );
+    }
+
     return (
-      <div className="div">LoginForm</div>
+      <div className="form-container signup-container">
+        <div>
+          <h1 className="header-ruler">Login</h1>
+          <p>Login or <Link to="/signup">click here to create an account</Link></p>
+        </div>
+        <form onSubmit={this.handleSubmit} className="signup-form-box">
+          {this.renderErrors()}
+          <div className="auth-form">
+
+            <div className="form-group">
+              <label htmlFor="form-email">Email</label>
+              <input type="text"
+                value={this.state.email}
+                onChange={this.update('email')}
+                className="form-input"
+                id="form-email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="form-password">Password</label>
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="form-input"
+                id="form-password"
+              />
+            </div>
+
+            <input className="signup-submit btn-primary" type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
--- app/javascript/packs/components/LoginForm/LoginFormContainer.js
+++ app/javascript/packs/components/LoginForm/LoginFormContainer.js
@@ -2,13 +2,19 @@ import { connect } from 'react-redux';
 import { LoginForm } from './LoginForm';
 
 // Actions
+import { login } from '../../actions/sessionActions';
+import { resetRedirect } from '../../actions/uiActions';
+import { resetSessionErrors } from '../../actions/sessionActions';
 
 const mapStateToProps = state => ({
-
+  errors: state.errors.sessionErrors,
+  redirect: state.ui.redirect
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  processForm: user => dispatch(login(user)),
+  resetRedirect: () => dispatch(resetRedirect()),
+  resetSessionErrors: () => dispatch(resetSessionErrors())
 });
 
 export default connect(
