3141b52e4356b416d63f99dedb71b43d7ef9ecf0
Mitul Mistry <MitulMistryDev@gmail.com>
2021-12-31 23:34:31 -0600

    Change parent components to use spread operator for props rather than including each prop explicitly

--- app/javascript/packs/components/ArtworkShow/ArtworkShowParent.jsx
+++ app/javascript/packs/components/ArtworkShow/ArtworkShowParent.jsx
@@ -5,15 +5,13 @@ import { ArtworkShow } from './ArtworkShow';
 // In order to get the route id using React Router v6, need to have a functional component
 // for useParams() hook. withRouter doesn't work. So for now, use this parent functional
 // component to pass the route id along with props from Redux container.
-export default function ArtworkShowParent({artworkShow, imageBaseUrl, fetchArtwork}) {
+export default function ArtworkShowParent(props) {
   const { artworkId } = useParams();
 
   return (
     <ArtworkShow
-      artworkShow={artworkShow}
-      imageBaseUrl={imageBaseUrl}
-      fetchArtwork={fetchArtwork}
       artworkId={parseInt(artworkId)}
+      {...props}
     />
   );
 }
--- app/javascript/packs/components/CollectionShow/CollectionShowParent.jsx
+++ app/javascript/packs/components/CollectionShow/CollectionShowParent.jsx
@@ -5,15 +5,13 @@ import { CollectionShow } from './CollectionShow';
 // In order to get the route id using React Router v6, need to have a functional component
 // for useParams() hook. withRouter doesn't work. So for now, use this parent functional
 // component to pass the route id along with props from Redux container.
-export default function ArtworkShowParent({collectionShow, loggedIn, fetchCollection}) {
+export default function CollectionShowParent(props) {
   const { collectionId } = useParams();
 
   return (
     <CollectionShow
-      collectionShow={collectionShow}
-      loggedIn={loggedIn}
-      fetchCollection={fetchCollection}
       collectionId={parseInt(collectionId)}
+      {...props}
     />
   );
 }
